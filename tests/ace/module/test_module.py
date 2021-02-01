7698e6883f17e6953873d818c781614e9b619043
John Davison <John.Davison@53.com>
2021-02-01 13:53:10 -0500

    forgot to add this file

---
+++ tests/ace/module/test_module.py
@@ -0,0 +1,35 @@
+# vim: ts=4:sw=4:et:cc=120
+#
+
+import multiprocessing
+
+from ace.analysis import AnalysisModuleType
+from ace.module.base import AnalysisModule, DEFAULT_ASYNC_LIMIT
+
+import pytest
+
+
+@pytest.mark.unit
+def test_analysis_module_constructor():
+    class AsyncAnalysisModule(AnalysisModule):
+        async def execute_analysis(self, root, observable, analysis):
+            pass
+
+    # default sync module
+    module = AnalysisModule()
+    assert isinstance(module.type, AnalysisModuleType)
+    assert module.limit == multiprocessing.cpu_count()
+    assert module.timeout is None
+
+    # default async module
+    module = AsyncAnalysisModule()
+    assert isinstance(module.type, AnalysisModuleType)
+    assert module.limit == DEFAULT_ASYNC_LIMIT
+    assert module.timeout is None
+
+    # parameters specified
+    amt = AnalysisModuleType("test", "")
+    module = AnalysisModule(amt, 1, 2)
+    assert module.type == amt
+    assert module.limit == 1
+    assert module.timeout == 2

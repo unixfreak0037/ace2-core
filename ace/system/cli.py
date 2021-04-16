b783cbddf0049917fc86ddb4f768430aac77c448
John Davison <unixfreak0037@gmail.com>
2021-04-15 20:43:17 -0400

    ported read observables from stdin

--- ace/system/cli.py
+++ ace/system/cli.py
@@ -1,6 +1,7 @@
 # vim: ts=4:sw=4:et:cc=120
 
 import tempfile
+import sys
 
 from ace.cli import get_cli_sp, display_analysis
 from ace.env import get_uri, get_api_key
@@ -24,6 +25,13 @@ async def analyze(args):
         return False
 
     targets = args.targets
+
+    # did we specify targets from stdin?
+    if args.from_stdin:
+        for o_value in sys.stdin:
+            # the type of observables coming in on stdin is also specified on the command line
+            targets.append(args.stdin_type)
+            targets.append(o_value.strip())
 
     is_local = True
     uri = get_uri()
@@ -55,6 +63,9 @@ async def analyze(args):
 
     root = system.new_root()
 
+    if args.analysis_mode:
+        root.analysis_mode = args.analysis_mode
+
     index = 0
     while index < len(args.targets):
         o_type = args.targets[index]
@@ -77,5 +88,18 @@ async def analyze(args):
 
 
 analyze_parser = get_cli_sp().add_parser("analyze", help="Analyze given observables.")
+# options on the RootAnalysis
+analyze_parser.add_argument("-m", "--analysis-mode", help="Sets the analysis mode of the root.")
+analyze_parser.add_argument(
+    "--from-stdin",
+    action="store_true",
+    default=False,
+    help="Read observables from standard input. Default observable type is file. Use --stdin-type to change the type.",
+)
+analyze_parser.add_argument(
+    "--stdin-type",
+    default="file",
+    help="Specify the observable type when reading observables from stdin. Defaults to file.",
+)
 analyze_parser.add_argument("targets", nargs="*", help="One or more pairs of indicator types and values.")
 analyze_parser.set_defaults(func=analyze)
